3c4b22d3c12a94d768ca2f0fe4853940f495f6b4
Lucile <lucile.baudouin@gmail.com>
2022-05-17 16:43:35 +0200

    Fix Dashboard

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,48 @@
+// React
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+// Components
+import Profil from '../components/Profil';
+import Account from '../components/Account';
+
+const Dashboard = () => {
+   const store = useSelector((state) => state);
+
+   return (
+      store.token && (
+         <HelmetProvider>
+            <Helmet>
+               <title>Argent Bank - Dashboard</title>
+            </Helmet>
+            <main className="main bg-dark">
+               <Profil />
+               <h2 className="sr-only">Accounts</h2>
+               <Account
+                  title="Argent Bank Checking (x8349)"
+                  amount="$2,082.79"
+                  amountDescr="Available Balance"
+                  linkPath="/transactions"
+                  transButton="View transactions"
+               />
+               <Account
+                  title="Argent Bank Savings (x6712)"
+                  amount="$10,928.42"
+                  amountDescr="Available Balance"
+                  linkPath="/transactions"
+                  transButton="View transactions"
+               />
+               <Account
+                  title="Argent Bank Credit Card (x8349)"
+                  amount="$184.30"
+                  amountDescr="Current Balance"
+                  linkPath="/transactions"
+                  transButton="View transactions"
+               />
+            </main>
+         </HelmetProvider>
+      )
+   );
+};
+
+export default Dashboard;
