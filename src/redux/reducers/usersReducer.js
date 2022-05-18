5498a67c8559b5f801a644c652973560f855611e
Lucile <lucile.baudouin@gmail.com>
2022-05-18 16:34:10 +0200

    Fix usersReducer

---
+++ src/redux/reducers/usersReducer.js
@@ -0,0 +1,69 @@
+/**
+ * @typedef {import("../actions/actions").actionsTypes} actionsTypes
+ */
+ import { initialState } from '../store/store';
+ import { clearStorage } from '../../service/storage';
+ import { setBearer } from '../../service/service';
+ 
+ /**
+  *
+  * @param {Object} state
+  * @param {Object} action
+  * @param {Object} action.payload
+  * @param {actionsTypes} action.type
+  * @returns
+  */ 
+ 
+ export function usersReducer(state = initialState, action) {
+    const { payload } = action;
+    switch (action.type) {
+       case 'LOADING_IN_PROGRESS':
+          return {
+             ...state,
+             currentState: 'loading',
+             loader: payload,
+          };
+       case 'LOGIN_SUCEED':
+          setBearer(action.payload.token);
+          return {
+             ...state,
+             loggedIn: true,
+             currentState: 'logged',
+             token: action.payload.token,
+             loader: true,
+          };
+       case 'USER_PROFILE':
+          return {
+             ...state,
+             user: payload.user,
+             loader: false,
+          };
+       case 'SAVE_SUCCEED':
+          console.log({
+             ...state,
+             user: {
+                ...state.user,
+                ...payload.user,
+             },
+          });
+          return {
+             ...state,
+             user: {
+                ...state.user,
+                ...payload.user,
+             },
+          };
+       case 'LOGIN_FAILED':
+          return {
+             ...state,
+             error: true,
+             loader: false,
+             currentState: 'failed',
+          };
+       case 'LOGOUT_ACTION':
+          clearStorage();
+          return initialState;
+       default:
+          return state;
+    }
+ } 
