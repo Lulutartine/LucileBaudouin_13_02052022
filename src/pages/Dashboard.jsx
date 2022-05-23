a629c2b4a7b3e385a3397a222a1f39a32fe22949
Lucile <lucile.baudouin@gmail.com>
2022-05-23 13:18:08 +0200

    Fix Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 // Components
 import Profil from '../components/Profil';
-import Acount from '../components/Acount';
+import Account from '../components/Account';
 
 const Dashboard = () => {
    const store = useSelector((state) => state);
@@ -18,21 +18,21 @@ const Dashboard = () => {
             <main className="main bg-dark">
                <Profil />
                <h2 className="sr-only">Accounts</h2>
-               <Acount
+               <Account
                   title="Argent Bank Checking (x8349)"
                   amount="$2,082.79"
                   amountDescr="Available Balance"
                   linkPath="/transactions"
                   transButton="View transactions"
                />
-               <Acount
+               <Account
                   title="Argent Bank Savings (x6712)"
                   amount="$10,928.42"
                   amountDescr="Available Balance"
                   linkPath="/transactions"
                   transButton="View transactions"
                />
-               <Acount
+               <Account
                   title="Argent Bank Credit Card (x8349)"
                   amount="$184.30"
                   amountDescr="Current Balance"
